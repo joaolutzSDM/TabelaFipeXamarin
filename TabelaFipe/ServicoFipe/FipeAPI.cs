d41f0f532e71e4ef02e42e4eb2abb5ebbaca0882
João Gabriel Messi Lutz <joao.lutz@aluno.ifsp.edu.br>
2019-12-07 12:08:39 -0300

    Commit inicial do projeto

---
+++ TabelaFipe/ServicoFipe/FipeAPI.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using TabelaFipe.ServicoFipe.Model;
+
+namespace TabelaFipe.ServicoFipe
+{
+    public class FipeAPI
+    {
+        private static String buscaMarcas = "http://fipeapi.appspot.com/api/1/carros/marcas.json";
+        private static String buscaModelos = "http://fipeapi.appspot.com/api/1/carros/veiculos/{0}.json";
+        private static String buscaAnoModelo = "http://fipeapi.appspot.com/api/1/carros/veiculo/{0}/{1}.json";
+        private static String buscaVeiculo = "http://fipeapi.appspot.com/api/1/carros/veiculo/{0}/{1}/{2}.json";
+
+        private static List<FipeModel> getFipeModelList(String url)
+        {
+            WebClient wc = new WebClient();
+            String json = wc.DownloadString(url);
+            List<FipeModel> fm = JsonConvert.DeserializeObject<List<FipeModel>>(json);
+            return fm;
+        }
+
+        private static FipeModel getFipeModel(String url)
+        {
+            WebClient wc = new WebClient();
+            String json = wc.DownloadString(url);
+            FipeModel fm = JsonConvert.DeserializeObject<FipeModel>(json);
+            return fm;
+        }
+
+        public static List<FipeModel> buscarMarcas()
+        {   
+            return getFipeModelList(buscaMarcas);
+        }
+
+        public static List<FipeModel> buscarModelos(String idMarca)
+        {
+            String url = String.Format(buscaModelos, idMarca);
+            return getFipeModelList(url);
+        }
+
+        public static List<FipeModel> buscarAnoModelo(String idMarca, String idModelo)
+        {
+            String url = String.Format(buscaAnoModelo, idMarca, idModelo);
+            return getFipeModelList(url);
+        }
+
+        public static FipeModel buscarVeiculo(String idMarca, String idModelo, String idAnoModelo)
+        {
+            String url = String.Format(buscaVeiculo, idMarca, idModelo, idAnoModelo);
+            return getFipeModel(url);
+        }
+
+    }
+}
---
+++ TabelaFipe/ServicoFipe/Model/FipeModel.cs
@@ -0,0 +1,26 @@
+﻿namespace TabelaFipe.ServicoFipe.Model
+{
+	public class FipeModel
+    {
+		public string name { get; set; }
+		public string fipe_name { get; set; }
+		public string fipe_marca { get; set; }
+		public int order { get; set; }
+		public string key { get; set; }
+		public string id { get; set; }
+		public string marca { get; set; }
+		public string fipe_codigo { get; set; }
+		public string veiculo { get; set; }
+		public string combustivel { get; set; }
+		public string ano_modelo { get; set; }
+		public string preco { get; set; }
+		public string referencia { get; set; }
+		public double time { get; set; }
+
+		public string getAno_modelo()
+		{
+			return !ano_modelo.Equals("32000") ? ano_modelo : "Zero KM";
+		}
+
+	}
+}
